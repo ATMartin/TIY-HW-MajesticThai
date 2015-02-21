86de4f2464e8b022b4da6c8e7a68c04586653e58
Alex Martin <martin.alex.t+github@gmail.com>
2015-02-21 14:14:13 -0500

    All functionality in place. Time to play dress up.

--- js/views-admin.js
+++ js/views-admin.js
@@ -32,9 +32,46 @@ var AllItemsView = Backbone.View.extend({
   }
 });
 
+var OrderListing = Backbone.View.extend({
+  tagName: 'tr',
+  template: _.template($('[data-template="order-row"]').text()),  
+  events: {
+    'click .toggle' : 'toggleDone',
+    'click .remove' : 'removeOrder'  
+  },
+  toggleDone: function() {
+    var state = this.model.get('completed') ? false : true;
+    this.model.set('completed', state);
+    this.model.save();  
+  },
+  removeOrder: function() {
+    this.model.destroy();
+  },
+  render: function() {
+    this.$el.append(this.template(this.model.toJSON()));
+    return this;  
+  }
+});
+
+var AllOrdersView = Backbone.View.extend({  
+  initialize: function() {
+    this.listenTo(this.collection, 'destroy sync add', this.render);
+  },
+  render: function() {
+    var self = this;
+    self.$el.empty();
+    self.collection.forEach(function(order) {
+      var orderListing = new OrderListing({model: order});
+      orderListing.render();
+      self.$el.append(orderListing.el);  
+    });  
+    return this;
+  }
+});
 
 $(document).ready(function() {
   var menuItems = new MenuItems();
+  var orders = new Orders();
   var newItemView = new NewItemView({
     el: '.new-menu-item',
     collection: menuItems
@@ -43,8 +80,16 @@ $(document).ready(function() {
     el: '.menu-items',
     collection: menuItems
   });
+  var allOrdersView = new AllOrdersView({
+    el: '.orders',
+    collection: orders
+  });
+
   menuItems.fetch().done(function() {
     newItemView.render();
     allItemsView.render();
+  });
+  orders.fetch().done(function() {
+    allOrdersView.render();  
   });
 });
